8f57e59f0b9629257c44d9b8c8031a9bf0bd4c24
Jethro Muller <git@jethromuller.co.za>
2015-03-14 15:47:10 +0200

    Add inital boid prototype code. The boids move.

---
+++ boid.lua
@@ -0,0 +1,53 @@
+Boid = {
+	x = 0,
+	y = 0,
+	rotation = 0,
+	speed = 0,
+}
+
+
+function Boid:new(obj, x, y, rotation, speed)
+	local obj = obj or {}
+	obj.__index = self
+	obj.x = x
+	obj.y = y
+	obj.rotation = rotation * (math.pi / 180)
+	obj.speed = speed
+	setmetatable(obj, self)
+	self.__index = self
+	return obj
+end
+
+function Boid:setRotation(newRotation)
+	radianRotation = newRotation * (math.pi / 180)
+	self.rotation = radianRotation
+end
+
+function Boid:applySpeed()
+	-- resolve into x and y components
+	xComp = self.speed * math.sin(self.rotation)
+	if (self.rotation > 0 and self.rotation < math.pi) then
+		xComp = -1 * xComp
+	end
+	yComp = self.speed * math.cos(self.rotation)
+	if (self.rotation > math.pi and self.rotation < (2 * math.pi)) then
+		yComp = -1 * yComp
+	end
+
+	self.x = self.x + xComp
+	if (self.x > config.window.width) then
+		self.x = 0
+	elseif (self.x < 0) then
+	    self.x = config.window.width
+	end
+	self.y = self.y + yComp
+	if (self.y > config.window.height) then
+		self.y = 0
+	elseif (self.y < 0) then
+	    self.y = config.window.height
+	end
+end
+
+function Boid:print()
+	print("x: "..self.x, "y: "..self.y, "rotation: "..self.rotation, "speed: "..self.speed)
+end
---
+++ main.lua
@@ -0,0 +1,67 @@
+require "boid"
+
+function initializeBoids(numBoids)
+  for i=1,numBoids do
+    yRand = love.math.random(1, config.window.height)
+    xRand = love.math.random(1, config.window.width)
+    rotRand = love.math.random(0, 360)
+    speedRand = love.math.random()
+
+    newBoid = Boid:new(nil, xRand, yRand, rotRand, speedRand)
+    table.insert(boidHolder, i, newBoid)
+  end
+  for i=1,numBoids do
+    boidHolder[i]:print()
+  end
+end
+
+function love.load()
+  bigFont = love.graphics.newFont(25)
+  smallFont = love.graphics.newFont(15)
+  started = false
+  boidHolder = {}
+  numBoids = 10
+  initializeBoids(numBoids)
+end
+
+function love.keypressed(key)
+  if key == "escape" then
+    love.event.quit()
+  elseif key == " " then
+    started = true
+  end
+end
+
+
+function love.update(dt)
+  if (started == false) then
+  else
+      for i=1,numBoids do
+        boidHolder[i]:applySpeed()
+      end  
+  end
+end
+
+function love.draw()
+  love.graphics.setBackgroundColor(39, 40, 34)
+  if (started == false) then
+    love.graphics.setColor(0, 255, 102)
+    love.graphics.setFont(bigFont)
+    love.graphics.print("Boids Prototype", 150, 200)
+    love.graphics.setFont(smallFont)
+    love.graphics.print("Space to start", 190, 230)
+  else
+    local width = love.graphics.getWidth()
+    local height = love.graphics.getHeight()
+    
+    for i=1,numBoids do
+      love.graphics.setColor(i*i*10, i*15, i*13)
+      vertices = {
+        boidHolder[i].x, boidHolder[i].y + 5,
+        boidHolder[i].x + 5, boidHolder[i].y - 5,
+        boidHolder[i].x - 5, boidHolder[i].y - 5
+      }
+      love.graphics.polygon("fill", vertices)
+    end
+  end
+end
