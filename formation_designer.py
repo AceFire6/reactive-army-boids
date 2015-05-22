2e057fe48527c1909f349389fb135c7dbaca6101
Jethro Muller <JethroMuller@gmail.com>
2015-05-22 02:06:43 +0200

    Add file reading and writing with a GUI interface. Fix mouse over colour change.

--- formation_designer.py
+++ formation_designer.py
@@ -1,3 +1,5 @@
+from Tkinter import *
+import tkFileDialog
 import pygame
 import config
 import math
@@ -7,7 +9,7 @@ boundaries = True
 
 
 def distance_to(point1, point2):
-    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+    return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
 
 
 def close_to(point, placed_units):
@@ -21,8 +23,10 @@ def get_closest(point, close_units):
         dist = distance_to(point, closest)
 
         for close_unit in close_units:
-            if distance_to(point, close_unit) < dist:
+            temp_dist = distance_to(point, close_unit)
+            if temp_dist < dist:
                 closest = close_unit
+                dist = temp_dist
         return closest
     return None
 
@@ -41,8 +45,33 @@ def events(placed_units):
         if event.type == pygame.QUIT:
             return False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_ESCAPE:
+                return False
+            elif event.key == pygame.K_SPACE:
                 boundaries = not boundaries
+            elif event.key == pygame.K_s and (pygame.key.get_mods() &
+                                              pygame.KMOD_CTRL):
+                options = {
+                    'filetypes': [('Formations', '.fm')],
+                    'initialfile': 'formation1.fm',
+                    'title': 'Save the formation'}
+                save_file = tkFileDialog.asksaveasfile('w', **options)
+                if save_file:
+                    for placed_unit in placed_units:
+                        save_file.write(str(placed_unit) + '\n')
+                    save_file.close()
+            elif event.key == pygame.K_o and (pygame.key.get_mods() &
+                                              pygame.KMOD_CTRL):
+                options = {
+                    'filetypes': [('Formations', '.fm')],
+                    'title': 'Open formation'}
+                open_file = tkFileDialog.askopenfile('r', **options)
+                if open_file:
+                    for line in open_file.readlines():
+                        new_point = eval(line)
+                        if not close_to(new_point, placed_units):
+                            placed_units.append(new_point)
+                    open_file.close()
         elif event.type == pygame.MOUSEBUTTONDOWN:
             states = pygame.mouse.get_pressed()
             config.debug_print("STATES:", states)
@@ -82,6 +111,9 @@ def main():
     screen = pygame.display.set_mode(
         (config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
     running = True
+
+    win = Tk()
+    win.withdraw()
 
     placed_units = []
 
